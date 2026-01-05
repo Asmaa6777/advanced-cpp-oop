40825fd61c4adb84e744f315e6f920ed0a6efe29
Asmaa Farouq <asmaafarouq649@gmail.com>
2026-01-05 06:02:01 +0000

    Add theory and resources for OOP concepts, including mind maps and encapsulation

---
+++ OOP_foundation/encapsulation.cpp
@@ -0,0 +1,14 @@
+/** 
+
+Encapsulation is a fundamental concept in object-oriented programming (OOP) 
+that involves bundling data and methods together within a class, 
+while restricting direct access to some of the class's components.
+This is typically achieved using access specifiers such as :
+public, private, and protected.
+1. public -> accessible outside the class
+2. private -> accessible only inside the class
+3. protected -> will be discussed later (after inheritance)
+
+**/
+
+include <iostream>
